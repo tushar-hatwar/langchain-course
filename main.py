7dbe636b1e58331f690d2262350ec2dd99b4de96
Tushar Hatwar <tushar_hatwar@epam.com>
2025-12-27 23:02:02 +0530

    Working code with local proxy

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from typing import List
 
 from pydantic import BaseModel, Field
-from dotenv import load_dotenv
-
-load_dotenv()
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+from azure_env import llm
 
 
 class Source(BaseModel):
@@ -26,7 +23,6 @@ class AgentResponse(BaseModel):
     )
 
 
-llm = ChatOpenAI(model="gpt-5")
 tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
@@ -34,10 +30,12 @@ agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 def main():
     print("Hello from langchain-course!")
     result = agent.invoke(
-        {
-            "messages": HumanMessage(
-                content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?"
-            )
+        input={
+            "messages": [
+                HumanMessage(
+                    content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?"
+                )
+            ]
         }
     )
     print(result)
